16d920477589bc845051e4f60d921e47ccc0436c
vitamin777 <rlaalsdn8@gmail.com>
2022-08-14 17:37:59 +0900

    refactor : 함수형 컴포넌트 User.js 를 클래스형 컴포넌트로 변환
    -변경 사유: 함수형 컴포넌트에서 클래스형 컴포넌트로 변환 연습
    
    -기존 작동 기능 정상 확인
    
    -gitignore setting
    
    -npm install

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,21 @@
+import {Component} from 'react'
 import classes from './User.module.css';
 
-const User = (props) => {
+/*class 예약어 클래스이름 클래스 정의 내용이있는 중괄호*/
+class User extends Component {
+
+  /*render method 는 리액트에 필요한 특정 메소드로 리액트가 jsx 코드 안에 컴포넌트가 사용된것을 확인하면 호출하는 메소드*/
+  render (){
+    return (
+        <li className={classes.user}>{this.props.name}</li> /*this 예약어를 가지고 props 에 접근*/
+    )
+  }
+}
+
+export default User;
+
+/*const User = (props) => {
   return <li className={classes.user}>{props.name}</li>;
 };
 
-export default User;
+export default User;*/
